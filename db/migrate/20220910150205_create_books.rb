b44c43e27d20bf63e133b5810dc56b8e0db97d39
kennbeckloff <kennbeckloff688@gmail.com>
2022-09-10 18:08:51 +0300

    restructure tables

--- db/migrate/20220910150205_create_books.rb
+++ db/migrate/20220910150205_create_books.rb
@@ -1,4 +1,16 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
+    create_table :books do |t|
+      t.string :title
+      t.string :authors
+      t.string :publisher
+      t.string :publishedDate
+      t.string :description
+      t.integer :pageCount
+      t.integer :averageRating
+      t.integer :ratingsCount
+      t.string :imageLinks #medium
+      t.string :language
+    end
   end
 end
--- db/migrate/20220910150232_create_reviews.rb
+++ db/migrate/20220910150232_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :reviews
+      t.integer :book_id
+      t.integer :user_id
+    end
   end
 end
