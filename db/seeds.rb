b9e64798db8076d31e2df434086ea9d26d6bf8a8
kennbeckloff <kennbeckloff688@gmail.com>
2022-09-10 18:48:15 +0300

    seeding to books

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,27 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+
+books.each do |book|
+    response = RestClient.get "https://www.googleapis.com/books/v1/volumes/#{book}"
+
+    book_hash = JSON.parse(response)
+
+    Book.create(
+        title: book_hash["volumeInfo"]["title"],
+        authors: book_hash["volumeInfo"]["authors"][0],
+        publisher: book_hash["volumeInfo"]["publisher"],
+        publishedDate: book_hash["volumeInfo"]["publishedDate"],
+        description: book_hash["volumeInfo"]["description"],
+        pageCount: book_hash["volumeInfo"]["pageCount"],
+        averageRating: book_hash["volumeInfo"]["averageRating"],
+        ratingsCount: book_hash["volumeInfo"]["ratingsCount"],
+        imageLinks: book_hash["volumeInfo"]["imageLinks"]["thumbnail"],
+        language: book_hash["volumeInfo"]["language"]
+    )
+end
 puts "✅ Done seeding!"
+
+
+
+
